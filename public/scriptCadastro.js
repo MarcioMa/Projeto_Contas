dfb31c4060ab30841c084bb8fe87cd399ade2be2
MarcioMa <marmac1979@gmail.com>
2024-07-24 18:54:30 -0300

    24/07/2024 18:33

--- public/scriptCadastro.js
+++ public/scriptCadastro.js
@@ -7,22 +7,22 @@ function novoReg(){
   const bntLeft = document.getElementById('bntLeft');
   const fieldset = document.getElementById('fldSet');
 
-    if(bntNovo.value === 'Novo') {
-      fieldset.disabled = false;  
-      bntNovo.value = 'Cancelar';
-      bntSave.disabled = false;
-      bntAtualizar.disabled = false;
-      bntExcluir.disabled = false;
-      bntRight.disabled = false;
-      bntLeft.disabled = false;
-    }else{
-      bntNovo.value = 'Novo';
-      bntSave.disabled = true;
-      fieldset.disabled = true; 
-      bntAtualizar.disabled = true;
-      bntExcluir.disabled = true;
-      bntRight.disabled = true;
-      bntLeft.disabled = true;      
-    }
+  if(bntNovo.value === 'Novo') {
+    fieldset.disabled = false;  
+    bntNovo.value = 'Cancelar';
+    bntSave.disabled = false;
+    bntAtualizar.disabled = false;
+    bntExcluir.disabled = false;
+    bntRight.disabled = false;
+    bntLeft.disabled = false;
+  }else{
+    bntNovo.value = 'Novo';
+    bntSave.disabled = true;
+    fieldset.disabled = true; 
+    bntAtualizar.disabled = true;
+    bntExcluir.disabled = true;
+    bntRight.disabled = true;
+    bntLeft.disabled = true;      
+  }
  
   }
